2b7b3f301557f5edfa83d25db8e4129b0da3008e
Batu <batuhan.guner@student.ehb.be>
2024-12-27 03:12:46 +0100

    Initial Commit

---
+++ src/main/java/com/batuhan/eindprojectBatuhan/EindprojectBatuhanApplication.java
@@ -0,0 +1,13 @@
+package com.batuhan.eindprojectBatuhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindprojectBatuhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindprojectBatuhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batuhan/eindprojectBatuhan/EindprojectBatuhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuhan.eindprojectBatuhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindprojectBatuhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
